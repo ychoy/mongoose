81ca265a2a2f22249747f415f47d8cbdc1449809
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2016-12-13 12:45:05 -0800

    Update index.js

--- starter-code/models/index.js
+++ starter-code/models/index.js
@@ -1,8 +1,16 @@
 var mongoose = require('mongoose');
+
+// use native JS promise library instead of Mongoose's deprecated one
+mongoose.Promise = global.Promise;
+
+// connect to the database todo-app
 mongoose.connect('mongodb://localhost/todo-app');
 
+
+// bring in models from separate model files
 var Person = require('./person');
 // var Todo = require('./todo');
 
+// export the models so they can be used elsewhere
 exports.Person = Person;
 // exports.Todo = Todo;
