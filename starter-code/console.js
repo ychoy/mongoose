b8055da6fb2ec0e9d80d8292a39694232e8ca965
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2016-12-13 12:08:01 -0800

    comments in console.js

--- starter-code/console.js
+++ starter-code/console.js
@@ -1,6 +1,14 @@
-var REPL = require("repl");
+// run this file with `node console.js`
+
+// helps set up the CLI
+var REPL = require("repl"); 
+
+// bring in all of the code from models/index.js
+// this allows us to access models with db.ModelName 
+// for example: db.Person, db.Author
 var db = require("./models");
 
+// the rest of this is setup for the CLI
 var repl = REPL.start("> ");
 repl.context.db = db;
 
